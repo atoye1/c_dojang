1089a0b6d76e8cdba3cf2f459983d7d987b2190d
atoye1 <ssalssi1@gmail.com>
2020-03-26 20:50:48 +0900

    chapter 42 string copy

---
+++ c_dojang/42/strin_copy_array_to_memory.c
@@ -0,0 +1,21 @@
+
+/* C template made by Seol*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main()
+{
+	char *s1 = "Hello";
+	char *s2= malloc(sizeof(char) * 20);
+	printf("so far good\n");
+	strcpy(s2, "world");
+
+	strcat(s2, s1);
+
+	printf("%s\n", s2);
+
+
+	return 0;
+}
---
+++ c_dojang/42/string_copy_array_to_array.c
@@ -0,0 +1,18 @@
+
+/* C template made by Seol*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	char s1[10] = "Hello";
+	char s2[1];
+
+	strcpy(s2, s1);
+
+	printf("%s\n", s2);
+
+	return 0;
+}
