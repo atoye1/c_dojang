1e6771e4cca1abbc2473d4c0dc07f6dc057bdd47
atoye1 <ssalssi1@gmail.com>
2020-04-06 21:58:00 +0900

    struct

--- c_dojang/42/string_copy_pointer_to_memory.c
+++ c_dojang/42/string_copy_pointer_to_memory.c
@@ -8,10 +8,11 @@
 int main()
 {
 	char *s1 = "Hello";
-	char *s2= malloc(sizeof(char) * 20);
-	printf("so far good\n");
-	strcpy(s2, s1);
+	char *s2 = malloc(sizeof(char) * 20);
 
+	printf("so far good\n");
+
+	strcpy(s2, s1);
 	strcat(s2, s1);
 
 	printf("%s\n", s2);
